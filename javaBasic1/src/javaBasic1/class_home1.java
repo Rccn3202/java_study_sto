2e993a49be6a9dda7e1da79dc25678159a98d2a4
comput3202 <comput3202@gmail.com>
2023-01-09 11:24:22 +0900

    2023.01.09

---
+++ javaBasic1/src/javaBasic1/class_home1.java
@@ -0,0 +1,14 @@
+package javaBasic1;
+
+public class class_home1 {
+
+	public static void main(String[] args) {
+		//p.131 예제: 1,2를 입력했을 때 속도를 증,감속 시키고 3 입력하면 프로그램 종료 시키는 예제
+				
+		//p.133 예제:
+				
+		//p.135 예제:
+
+	}
+
+}
